8f80e9c1f3850ce532d76dec914804ea63a1fd58
minsuklee80 <minsuklee80@naver.com>
2017-04-23 23:03:42 +0900

    170422

---
+++ practice/practice_구구단(ms).py
@@ -0,0 +1,17 @@
+for x in range(2,3):
+    for y in range(1, 10):
+        print("%d x %d = %2d\t"
+              "%d x %d = %2d\t"
+              "%d x %d = %2d\t"
+              "%d x %d = %2d"
+              %(x,y,x*y,x+1,y,(x+1)*y,
+              x+2,y,(x+2)*y,x+3,y,(x+3)*y))
+print('')
+for x in range(6,7):
+    for y in range(1, 10):
+        print("%d x %d = %2d\t"
+              "%d x %d = %2d\t"
+              "%d x %d = %2d\t"
+              "%d x %d = %2d"
+              %(x,y,x*y,x+1,y,(x+1)*y,
+              x+2,y,(x+2)*y,x+3,y,(x+3)*y))
--- practice/practice_길이50인문자열생성_string,choice.py
+++ practice/practice_길이50인문자열생성_string,choice.py
@@ -1,6 +1,47 @@
 >>> #길이가 50인 임의의 문자열 만들기
 >>> import random
 >>> import string
+>>> help(string)
+Help on module string:
+
+NAME
+    string - A collection of string constants. #정수의 콜렉션
+
+DESCRIPTION #기술
+    Public module variables:
+
+    whitespace -- a string containing all ASCII whitespace
+	#모든 ASCII 공백이 포함 된 문자열
+    ascii_lowercase -- a string containing all ASCII lowercase letters
+	#모든 ASCII 소문자를 포함하는 문자열
+    ascii_uppercase -- a string containing all ASCII uppercase letters
+	#모든 ASCII 대문자를 포함하는 문자열
+    ascii_letters -- a string containing all ASCII letters
+	#모든 ASCII 문자가 포함 된 문자열
+    digits -- a string containing all ASCII decimal digits
+	#모든 ASCII 10 진수를 포함하는 문자열
+    hexdigits -- a string containing all ASCII hexadecimal digits
+	#모든 ASCII 16 진수가 포함 된 문자열
+    octdigits -- a string containing all ASCII octal digits
+	#모든 ASCII 8 진수를 포함하는 문자열
+    punctuation -- a string containing all ASCII punctuation characters
+	#모든 ASCII 구두점 문자를 포함하는 문자열
+    printable -- a string containing all ASCII characters considered printable
+	#인쇄 가능한 것으로 간주되는 모든 ASCII 문자가 포함 된 문자열
+
+DATA
+    __all__ = ['ascii_letters', 'ascii_lowercase', 'ascii_uppercase', 'cap...
+    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    digits = '0123456789'
+    hexdigits = '0123456789abcdefABCDEF'
+    octdigits = '01234567'
+    printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU...
+    punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    whitespace = ' \t\n\r\x0b\x0c'
+
+	
 >>> dir(string)
 ['Formatter', 'Template', '_ChainMap', '_TemplateMetaclass', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', '_re', '_string', 'ascii_letters', 'ascii_lowercase', 'ascii_uppercase', 'capwords', 'digits', 'hexdigits', 'octdigits', 'printable', 'punctuation', 'whitespace']
 >>> a=string.ascii_letters
