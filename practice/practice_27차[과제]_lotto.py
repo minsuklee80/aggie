4da0b680a78039e46605fc5a3067ee4a0551f4be
minsuklee80 <minsuklee80@naver.com>
2017-05-10 20:49:43 +0900

    .

--- practice/practice_27차[과제]_lotto.py
+++ practice/practice_27차[과제]_lotto.py
@@ -2,6 +2,11 @@ import random
 
 lotto_mynum = []
 
+lotto = set(range(1,46))                     #로또 전체 번호
+num1 = set(random.sample(range(1,46), 6))    #로또 1등 번호
+num2 = set(random.sample((lotto - num1), 1)) #로또 2등 보너스 번호
+print("이번 로또 번호는 \n%s이고,\n보너스 번호는%s 입니다." %(num1, num2))
+
 for i in range(1,7):
     mynum = int(input("%d번째 로또 번호를 입력하세요: " %i))
     while mynum > 45:     #45보다 큰 번호는 오류를 표시
@@ -13,38 +18,22 @@ if len(lotto_mynum) == 5:
     mynum = input("로또 번호를 하나 더 입력하세요: ")
     lotto_mynum.append(mynum)
     
-<<<<<<< HEAD
-print("당신이 선택한 번호는\n%d 입니다." %set(lotto_mynum))
-
-lotto = set(range(1,46))                     #로또 전체 번호
-num1 = set(random.sample(range(1,46), 6))    #로또 1등 번호
-num2 = set(random.sample((lotto - num1), 1)) #로또 2등 보너스 번호
-print("이번 로또 번호는 /n%d이고,\n보너스 번호는%d 입니다." %(num1, num2))
+print("당신이 선택한 번호는\n%s 입니다." %set(lotto_mynum))
 
 lotto_num1 = len(num1 & set(lotto_mynum))
-lotto_num2 = num2 & set(lotto_mynum)
+lotto_num2 = len(num2 & set(lotto_mynum))
 
 if lotto_num1 == 6:
     print("축하합니다. 1등에 당첨되었습니다.")
+elif lotto_num1 == 5 and lotto_num2 == 1:
+    print("축하합니다. 2등에 당첨되었습니다.")
 elif lotto_num1 == 5:
-    if len(num2 & lotto_num2) == 1:
-       print("축하합니다. 2등에 당첨되었습니다.")
-    else:
-        print("축하합니다. 3등에 당첨되었습니다.")   
+    print("축하합니다. 3등에 당첨되었습니다.")
 elif lotto_num1 == 4:
-    print("축하합니다. 4등에 당첨되었습니다./n")
+    print("축하합니다. 4등에 당첨되었습니다.")
 else:
-    print("꽝")
-=======
-print(input("당신이 선택한 번호는 %s 입니다. " %set(lotto_mynum)))
-lotto_mynum1 = set(lotto_mynum)
-lotto_num_a = len(lotto_num1 & lotto_mynum1)
-lotto_num_b = len(lotto_num2 & lotto_mynum2)
+    print("아쉽네요. 꽝! 입니다.")
 
-if lotto_num_a == 6:
-    print("축하합니다. 1등에 당첨되셨습니다.")
-elif (lotto_num_a == 5) and (lotto_num_b == 1):
-    print("축하합니다. 2등에 당첨되셨습니다.")
->>>>>>> origin/master
+
 
 
